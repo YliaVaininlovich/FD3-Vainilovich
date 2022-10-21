6dd0e6554db156c23ca7759346dc6e9f61eeec0b
YliaVaininlovich <ylia.v@tut.by>
2022-10-21 23:29:17 +0300

    feat: add component Product

--- ishop/Product.js
+++ ishop/Product.js
@@ -1,10 +1,39 @@
 const Product = React.createClass({
   displayName: "Product",
   render: function () {
+    var productCard = [];
+
+    for (let i = 0; i < this.props.products.length; i++) {
+      var element = this.props.products[i];
+      var productCode = React.DOM.div(
+        {
+          key: element.key,
+          className: "ProductDescription",
+        },
+        React.DOM.img({
+          className: "ProductDescriptionPhoto",
+          src: element.photo,
+        }),
+        React.DOM.div(
+          { className: "ProductDescription" },
+          "Наименование:  " + element.name
+        ),
+        React.DOM.div(
+          { className: "ProductDescription" },
+          "Цена:   " + element.price
+        ),
+        React.DOM.div(
+          { className: "ProductDescription" },
+          "Остаток на складе:  " + element.quantity
+        )
+      );
+      productCard.push(productCode);
+    }
+
     return React.DOM.div(
-      { className: "ProductFrame" },
-      React.DOM.h1(null, "Всем привет!!!"),
-      React.DOM.div({ className: "MyComponentText" }, "wertysdcfvxcvb")
+      { className: "StoreName" },
+      React.DOM.h1(null, this.props.store),
+      React.DOM.div({ className: "ProductContainer" }, productCard)
     );
   },
 });
